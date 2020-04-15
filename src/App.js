6b5a1a3ef2ca4f483707cbe0f7bbc7910410f0b1
Josué Llopez <llopezjosue31@gmail.com>
2020-04-15 10:20:15 +0200

    Structure de base réalisée

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import './components/Home';
 import './App.css';
+import { Header } from './components/Header';
+import Home from './components/Home';
+import { Banner } from './components/Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home></Home> 
     </div>
   );
 }
