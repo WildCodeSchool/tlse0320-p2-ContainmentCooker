9d625bb465011e6aa9e74e80526a721262f0ecce
Josué Llopez <llopezjosue31@gmail.com>
2020-04-16 15:35:57 +0200

    Banner presque terminée (titre ok, background ok, responsive ok, recette ok)

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-// import img from './BannerImg.jpg';
-import './Banner.css';
+import './Css/Banner.css';
+import Grill from './grill.png';
+import Ham from './ham.png';
+import Ribs from './ribs.png';
 
 function Banner() {
   return (
     <div className="Bann">
-      <div className="Banner">
-        <h1 id="Title"> Grilled Meat Skewers </h1>
+      <div className="BannerUp">
+        <div className="TitleButton">
+          <h1 className="TitleRecipe"> Grilled Meat Skewers </h1>
+          <button type="button" className="ButtonBanner">
+            See the recipe
+          </button>
+        </div>
+        <div className="LogosBanner">
+          <img src={Grill} className="GrillLogo" alt="" />
+          <img src={Ham} className="HamLogo" alt="" />
+          <img src={Ribs} className="RibsLogo" alt="" />
+        </div>
       </div>
       <div className="BannerBottom">
         <h2 className="TitleBanner">Recipes</h2>
