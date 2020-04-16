b69a6b0b07114ae8ebd4b28e359e8fa01f2431cc
thierry arzal <thierryarzal353@gmail.com>
2020-04-16 15:53:53 +0200

    Second commit of Header still working on it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './components/Home';
 import './App.css';
-import Header from './components/Header';
+import Home from './components/Home';
+// import Header from './components/Header';
+// import { Button } from 'reactstrap';
 
 // import { Header } from './components/Header';
 // import Home from './components/Home';
@@ -10,7 +12,7 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Home />
       {/* <Home></Home>
        */}
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
 
-const Header = () => (
-  <nav className="Navbar">
-    <a href="https://fr.wikipedia.org/wiki/Les_Simpson">Quotes</a>
-  </nav>
-);
+const Header = props => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" dark expand="md">
+        <NavbarBrand href="/">Containment Cooker</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Recipes</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">Ingredients</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Profil</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
 
 export default Header;
