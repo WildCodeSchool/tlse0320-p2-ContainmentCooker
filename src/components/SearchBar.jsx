9423d66c3288d6a8d21c3f729e97534738659982
Alexandra31150 <alexandrap44@gmail.com>
2020-04-16 18:24:12 +0200

    mise en place media queries ( a terminer )

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,18 +4,17 @@ import './SearchBar.css';
 function SearchBar() {
   return (
     <div className="SearchBar">
-      <div id="container">
-        <div id="searcharea">
-          <input type="text" placeholder="Enter 3 ingredients, I give you 5 recipes" id="bar" />
-          <img
-            src="https://pngimage.net/wp-content/uploads/2018/06/icone-loupe-png-2.png"
-            id="searchsubmit"
-            alt="loupe"
-          />
-        </div>
-        <div />
-        <input type="submit" value="Advanced research" id="advancedbutton" />
+      <h2>Enter 3 ingredients, I give you 5 recipes</h2>
+      <div id="searcharea">
+        <input type="text" placeholder="Your ingredients" id="bar" />
+        <img
+          src="https://pngimage.net/wp-content/uploads/2018/06/icone-loupe-png-2.png"
+          id="searchsubmit"
+          alt="loupe"
+        />
       </div>
+      <div />
+      <input type="button" value="Advanced research" id="advancedbutton" />
     </div>
   );
 }
