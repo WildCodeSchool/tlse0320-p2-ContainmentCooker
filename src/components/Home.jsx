21bc68cdba44bc65925fda85347a59443ff97b6a
TOBOTOMAS31 <thomas.couderc5@gmail.com>
2020-04-17 11:58:53 +0200

    resolution pb

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,12 @@
 import React from 'react';
-/*import './Advert';
-import './Banner';
-import './CategoriesList';
-import './Footer';
-import './Header';
-import './SearchBar';
-import { Header } from './Header';
-import { Banner } from './Banner';
-import { SearchBar } from './SearchBar';
-import { Advert } from './Advert';
-*/
-import { CategoriesList } from './CategoriesList';
+import CategoriesList from './CategoriesList';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        {/*
-        <Header/>
-        <Banner/>
-        <SearchBar/>
-        <Advert/>
-        <Footer/>
-        */}
-
-        <CategoriesList />
-      </div>
-    );
-  }
+function Home() {
+  return (
+    <div>
+      <CategoriesList />
+    </div>
+  );
 }
 
 export default Home;
