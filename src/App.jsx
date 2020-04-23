1274ac37fdf2c3bfbe5ea800acab748b810a5f1b
Josué Llopez <llopezjosue31@gmail.com>
2020-04-23 10:53:44 +0200

    création de la page contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import RandomPage from './components/RandomPage';
+import ContactPage from './components/ContactPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/randompage" component={RandomPage} />
+          <Route path="/contact" component={ContactPage} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/components/ContactPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+function Home() {
+  return (
+    <div className="global">
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
