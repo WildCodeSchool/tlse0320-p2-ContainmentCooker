5f5b5120514bc20841c263579652d6b917be94aa
Josué Llopez <llopezjosue31@gmail.com>
2020-04-23 11:08:05 +0200

    réalisation du formulaire de contact

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import ContactForm from './ContactForm';
 
 function Home() {
   return (
     <div className="global">
       <Header />
+      <ContactForm />
     </div>
   );
 }
