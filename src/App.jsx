a9411f1d2f886b5ffbf20b083a920fbedad01b95
TOBOTOMAS31 <thomas.couderc5@gmail.com>
2020-05-06 17:15:57 +0200

    encore des tests

--- src/App.jsx
+++ src/App.jsx
@@ -13,30 +13,30 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [country, handleCountry] = useState('');
-  const [category, handleCategory] = useState('');
-  const [recipesByCountry, setRecipesByCountry] = useState([]);
-  const [recipesByCategory, setRecipesByCategory] = useState([]);
-  const [recipesResults, setRecipesResults] = useState([]);
+  // const [country, handleCountry] = useState('');
+  // const [category, handleCategory] = useState('');
+  // const [recipesByCountry, setRecipesByCountry] = useState([]);
+  // const [recipesByCategory, setRecipesByCategory] = useState([]);
+  // const [recipesResults, setRecipesResults] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${country}`)
-      .then(res => res.data.meals)
-      .then(res => setRecipesByCountry(res));
-  }, [country]);
+  // useEffect(() => {
+  //   axios
+  //     .get(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${country}`)
+  //     .then(res => res.data.meals)
+  //     .then(res => setRecipesByCountry(res));
+  // }, [country]);
 
-  useEffect(() => {
-    axios
-      .get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
-      .then(res => res.data.meals)
-      .then(res => setRecipesByCategory(res));
-  }, [category]);
+  // useEffect(() => {
+  //   axios
+  //     .get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
+  //     .then(res => res.data.meals)
+  //     .then(res => setRecipesByCategory(res));
+  // }, [category]);
 
-  useEffect(() => {
-    const results = _.intersectionWith(recipesByCountry, recipesByCategory, _.isEqual);
-    setRecipesResults(results);
-  }, [recipesByCategory, recipesByCountry]);
+  // useEffect(() => {
+  //   const results = _.intersectionWith(recipesByCountry, recipesByCategory, _.isEqual);
+  //   setRecipesResults(results);
+  // }, [recipesByCategory, recipesByCountry]);
 
   return (
     <div className="App">
@@ -45,19 +45,23 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/randompage" component={RandomPage} />
           <Route path="/contact" component={ContactPage} />
-          <Route path="/list-categories/:strCategorie" component={ListPage} />
+          <Route
+            path="/list-categories/:strCategorie"
+            component={ListPage}
+            // component={props => ( <ListPage {...props} country={country} handleCountry={handleCountry} />)}
+          />
           <Route path="/list-recipes/:strIngredient" component={ListPage} />
           <Route
             path="/all-recipes"
-            component={() => (
-              <AdvancedSearch
-                handleCountry={handleCountry}
-                country={country}
-                handleCategory={handleCategory}
-                category={category}
-                recipesResults={recipesResults}
-              />
-            )}
+            // component={() => (
+            //   <AdvancedSearch
+            //     handleCountry={handleCountry}
+            //     country={country}
+            //     handleCategory={handleCategory}
+            //     category={category}
+            //     recipesResults={recipesResults}
+            //   />
+            // )}
           />
         </Switch>
       </BrowserRouter>
--- src/components/ListPage.jsx
+++ src/components/ListPage.jsx
@@ -8,10 +8,41 @@ import _ from 'lodash';
 import AdvancedSearch from './AdvancedSearch';
 
 export default function ListPage() {
+  const [country, handleCountry] = useState('');
+  const [category, handleCategory] = useState('');
+  const [recipesByCountry, setRecipesByCountry] = useState([]);
+  const [recipesByCategory, setRecipesByCategory] = useState([]);
+  const [recipesResults, setRecipesResults] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${country}`)
+      .then(res => res.data.meals)
+      .then(res => setRecipesByCountry(res));
+  }, [country]);
+
+  useEffect(() => {
+    axios
+      .get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
+      .then(res => res.data.meals)
+      .then(res => setRecipesByCategory(res));
+  }, [category]);
+
+  useEffect(() => {
+    const results = _.intersectionWith(recipesByCountry, recipesByCategory, _.isEqual);
+    setRecipesResults(results);
+  }, [recipesByCategory, recipesByCountry]);
+
   return (
     <div className="listPage">
       <Header />
-      <AdvancedSearch />
+      <AdvancedSearch
+        handleCountry={handleCountry}
+        country={country}
+        handleCategory={handleCategory}
+        category={category}
+        recipesResults={recipesResults}
+      />
       <RecipesList />
       <Footer />
     </div>
