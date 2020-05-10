da02d8c64115a9f0a8b806c3dcfcd71291c53c55
TOBOTOMAS31 <thomas.couderc5@gmail.com>
2020-05-10 19:10:22 +0200

    on s'assure d'avoir tout commit

--- src/components/AdvancedResultsRecipesList.jsx
+++ src/components/AdvancedResultsRecipesList.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './Css/RecipesList.css';
 import { Row, Container } from 'reactstrap';
 import CardDetail from './CardDetail';
+import './Css/RecipesList.css';
 
 export default function AdvancedResultsRecipesList({
   country,
@@ -24,94 +25,106 @@ export default function AdvancedResultsRecipesList({
     <div>
       {rSelected === 1 ? (
         <Container className="main-container" fluid={true}>
-          <Row xs="1" sm="2" md="3" xl="4">
-            {country && recipesByCountry && category && recipesByCategory ? (
-              recipesResults.map(recipeResult => (
-                <CardDetail key={recipeResult.id} {...recipeResult} />
-              ))
-            ) : country &&
-              recipesByCountry &&
-              (!category || category === 'Choose your category') ? (
-              recipesByCountry.map(recipeResult => (
-                <CardDetail key={recipeResult.id} {...recipeResult} />
-              ))
-            ) : category && recipesByCategory && (!country || country === 'Choose your country') ? (
-              recipesByCategory.map(recipeResult => (
-                <CardDetail key={recipeResult.id} {...recipeResult} />
-              ))
-            ) : (!country || country === 'Choose your country') &&
-              (!category || category === 'Choose your category') ? (
-              allRecipes.map(recipeResult => <CardDetail key={recipeResult.id} {...recipeResult} />)
-            ) : (
-              <></>
-            )}
-          </Row>
+          <div className="recipes-list-container">
+            <Row xs="1" sm="2" md="3" xl="4">
+              {country && recipesByCountry && category && recipesByCategory ? (
+                recipesResults.map(recipeResult => (
+                  <CardDetail key={recipeResult.id} {...recipeResult} />
+                ))
+              ) : country &&
+                recipesByCountry &&
+                (!category || category === 'Choose your category') ? (
+                recipesByCountry.map(recipeResult => (
+                  <CardDetail key={recipeResult.id} {...recipeResult} />
+                ))
+              ) : category &&
+                recipesByCategory &&
+                (!country || country === 'Choose your country') ? (
+                recipesByCategory.map(recipeResult => (
+                  <CardDetail key={recipeResult.id} {...recipeResult} />
+                ))
+              ) : (!country || country === 'Choose your country') &&
+                (!category || category === 'Choose your category') ? (
+                allRecipes.map(recipeResult => (
+                  <CardDetail key={recipeResult.id} {...recipeResult} />
+                ))
+              ) : (
+                <></>
+              )}
+            </Row>
+          </div>
         </Container>
       ) : rSelected === 2 ? (
         <Container className="main-container" fluid={true}>
-          <Row xs="1" sm="2" md="3" xl="4">
-            {ingredient1 &&
-            recipesIngredient1 &&
-            ingredient2 === '' &&
-            (recipesIngredient2.length === 0 || recipesIngredient2 === null) &&
-            ingredient3 === '' &&
-            (recipesIngredient3.length === 0 || recipesIngredient3 === null) ? (
-              recipesIngredient1.map(recipeResult => (
-                <CardDetail key={recipeResult.id} {...recipeResult} />
-              ))
-            ) : ingredient1 &&
+          <div className="recipes-list-container">
+            <Row xs="1" sm="2" md="3" xl="4">
+              {ingredient1 &&
               recipesIngredient1 &&
-              ingredient2 &&
-              recipesIngredient2 &&
+              ingredient2 === '' &&
+              (recipesIngredient2.length === 0 || recipesIngredient2 === null) &&
               ingredient3 === '' &&
               (recipesIngredient3.length === 0 || recipesIngredient3 === null) ? (
-              recipesResultsInput1andInput2.map(recipeResult => (
-                <CardDetail key={recipeResult.id} {...recipeResult} />
-              ))
-            ) : ingredient1 &&
-              recipesIngredient1 &&
-              ingredient2 &&
-              recipesIngredient2 &&
-              ingredient3 === '' &&
-              (recipesIngredient3.length === 0 || recipesIngredient3 === null) &&
-              (recipesResultsInput1andInput2.length === 0 ||
-                recipesResultsInput1andInput2 === null) ? (
-              recipesIngredient1.map(recipeResult => (
-                <CardDetail key={recipeResult.id} {...recipeResult} />
-              ))
-            ) : ingredient1 &&
-              recipesIngredient1 &&
-              ingredient2 &&
-              recipesIngredient2 &&
-              ingredient3 &&
-              recipesIngredient3 ? (
-              recipesIngredientResults.map(recipeResult => (
-                <CardDetail key={recipeResult.id} {...recipeResult} />
-              ))
-            ) : ingredient1 === '' && ingredient2 === '' && ingredient3 === '' ? (
-              allRecipes.map(recipeResult => <CardDetail key={recipeResult.id} {...recipeResult} />)
-            ) : ingredient1 &&
-              recipesIngredient1 &&
-              ingredient2 &&
-              recipesIngredient2 &&
-              ingredient3 &&
-              recipesIngredient3 &&
-              recipesResults.lenght === 0 ? (
-              <div>
-                No results for : {ingredient1}, {ingredient2}, {ingredient3}.
-              </div>
-            ) : (
-              <h1></h1>
-            )}
-          </Row>
+                recipesIngredient1.map(recipeResult => (
+                  <CardDetail key={recipeResult.id} {...recipeResult} />
+                ))
+              ) : ingredient1 &&
+                recipesIngredient1 &&
+                ingredient2 &&
+                recipesIngredient2 &&
+                ingredient3 === '' &&
+                (recipesIngredient3.length === 0 || recipesIngredient3 === null) ? (
+                recipesResultsInput1andInput2.map(recipeResult => (
+                  <CardDetail key={recipeResult.id} {...recipeResult} />
+                ))
+              ) : ingredient1 &&
+                recipesIngredient1 &&
+                ingredient2 &&
+                recipesIngredient2 &&
+                ingredient3 === '' &&
+                (recipesIngredient3.length === 0 || recipesIngredient3 === null) &&
+                (recipesResultsInput1andInput2.length === 0 ||
+                  recipesResultsInput1andInput2 === null) ? (
+                recipesIngredient1.map(recipeResult => (
+                  <CardDetail key={recipeResult.id} {...recipeResult} />
+                ))
+              ) : ingredient1 &&
+                recipesIngredient1 &&
+                ingredient2 &&
+                recipesIngredient2 &&
+                ingredient3 &&
+                recipesIngredient3 ? (
+                recipesIngredientResults.map(recipeResult => (
+                  <CardDetail key={recipeResult.id} {...recipeResult} />
+                ))
+              ) : ingredient1 === '' && ingredient2 === '' && ingredient3 === '' ? (
+                allRecipes.map(recipeResult => (
+                  <CardDetail key={recipeResult.id} {...recipeResult} />
+                ))
+              ) : ingredient1 &&
+                recipesIngredient1 &&
+                ingredient2 &&
+                recipesIngredient2 &&
+                ingredient3 &&
+                recipesIngredient3 &&
+                recipesResults.lenght === 0 ? (
+                <div>
+                  No results for : {ingredient1}, {ingredient2}, {ingredient3}.
+                </div>
+              ) : (
+                <h1></h1>
+              )}
+            </Row>
+          </div>
         </Container>
       ) : rSelected !== 1 && rSelected !== 2 ? (
         <Container className="main-container" fluid={true}>
-          <Row xs="1" sm="2" md="3" xl="4">
-            {allRecipes.map(recipeResult => (
-              <CardDetail key={recipeResult.id} {...recipeResult} />
-            ))}
-          </Row>
+          <div className="recipes-list-container">
+            <Row xs="1" sm="2" md="3" xl="4">
+              {allRecipes.map(recipeResult => (
+                <CardDetail key={recipeResult.id} {...recipeResult} />
+              ))}
+            </Row>
+          </div>
         </Container>
       ) : (
         <></>
