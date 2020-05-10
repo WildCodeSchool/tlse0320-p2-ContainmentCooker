246eef4009a77a44d8e49ec6033907a7aed152b1
TOBOTOMAS31 <thomas.couderc5@gmail.com>
2020-05-10 18:05:05 +0200

    mise en place du state et des méthodes pour la recherche multi-critère par ingrédients

--- src/components/AdvancedSearchPage.jsx
+++ src/components/AdvancedSearchPage.jsx
@@ -5,7 +5,9 @@ import Footer from './Footer';
 import './Css/ListPage.css';
 import axios from 'axios';
 import _ from 'lodash';
+import { Button, ButtonGroup } from 'reactstrap';
 import AdvancedSearch from './AdvancedSearch';
+import InMyFridge from './InMyFridge';
 
 export default function AdvancedSearchPage() {
   const [country, handleCountry] = useState('');
@@ -14,6 +16,14 @@ export default function AdvancedSearchPage() {
   const [recipesByCategory, setRecipesByCategory] = useState([]);
   const [recipesResults, setRecipesResults] = useState([]);
   const [allRecipes, getAllRecipes] = useState([]);
+  const [ingredient1, handleIngredient1] = useState('');
+  const [ingredient2, handleIngredient2] = useState('');
+  const [ingredient3, handleIngredient3] = useState('');
+  const [recipesIngredient1, setRecipesIngredient1] = useState([]);
+  const [recipesIngredient2, setRecipesIngredient2] = useState([]);
+  const [recipesIngredient3, setRecipesIngredient3] = useState([]);
+  const [recipesResultsInput1andInput2, setRecipesResultsInput1andInput2] = useState([]);
+  const [recipesIngredientResults, setrecipesIngredientResults] = useState([]);
 
   useEffect(() => {
     axios
@@ -41,25 +51,111 @@ export default function AdvancedSearchPage() {
       .then(res => getAllRecipes(res));
   }, [allRecipes]);
 
+  useEffect(() => {
+    if (ingredient1 === '') {
+      setRecipesIngredient1([]);
+    } else {
+      axios
+        .get(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient1}`)
+        .then(res => res.data.meals)
+        .then(res => setRecipesIngredient1(res));
+    }
+  }, [ingredient1]);
+
+  useEffect(() => {
+    if (ingredient2 === '') {
+      setRecipesIngredient2([]);
+    } else {
+      axios
+        .get(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient2}`)
+        .then(res => res.data.meals)
+        .then(res => setRecipesIngredient2(res));
+    }
+  }, [ingredient2]);
+
+  useEffect(() => {
+    if (ingredient3 === '') {
+      setRecipesIngredient3([]);
+    } else {
+      axios
+        .get(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient3}`)
+        .then(res => res.data.meals)
+        .then(res => setRecipesIngredient3(res));
+    }
+  }, [ingredient3]);
+
+  useEffect(() => {
+    const results = _.intersectionWith(
+      recipesIngredient1,
+      recipesIngredient2,
+      recipesIngredient3,
+      _.isEqual
+    );
+    setrecipesIngredientResults(results);
+  }, [recipesIngredient1, recipesIngredient2, recipesIngredient3]);
+
+  useEffect(() => {
+    const results = _.intersectionWith(recipesIngredient1, recipesIngredient2, _.isEqual);
+    setRecipesResultsInput1andInput2(results);
+  }, [recipesIngredient1, recipesIngredient2]);
+
+  const [rSelected, setRSelected] = useState(null);
+
   return (
     <div>
       <Header />
-      <AdvancedSearch
-        handleCountry={handleCountry}
-        country={country}
-        handleCategory={handleCategory}
-        category={category}
-        recipesResults={recipesResults}
-      />
+
+      <ButtonGroup>
+        <Button color="primary" onClick={() => setRSelected(1)} active={rSelected === 1}>
+          Search by country and categorie
+        </Button>
+        <Button color="primary" onClick={() => setRSelected(2)} active={rSelected === 2}>
+          Search by ingredients
+        </Button>
+      </ButtonGroup>
+      {rSelected !== 1 && rSelected !== 2 ? (
+        <h2>Make your research choice</h2>
+      ) : rSelected === 1 ? (
+        <AdvancedSearch
+          handleCountry={handleCountry}
+          country={country}
+          handleCategory={handleCategory}
+          category={category}
+          recipesResults={recipesResults}
+        />
+      ) : rSelected === 2 ? (
+        <InMyFridge
+          ingredient1={ingredient1}
+          ingredient2={ingredient2}
+          ingredient3={ingredient3}
+          handleIngredient1={handleIngredient1}
+          handleIngredient2={handleIngredient2}
+          handleIngredient3={handleIngredient3}
+          recipesIngredient1={recipesIngredient1}
+          recipesIngredient2={recipesIngredient2}
+          recipesIngredient3={recipesIngredient3}
+          recipesIngredientResults={recipesIngredientResults}
+          recipesResultsInput1andInput2={recipesResultsInput1andInput2}
+        />
+      ) : (
+        <></>
+      )}
       <AdvancedResultsRecipesList
-        handleCountry={handleCountry}
         country={country}
-        handleCategory={handleCategory}
         category={category}
         recipesResults={recipesResults}
         recipesByCategory={recipesByCategory}
         recipesByCountry={recipesByCountry}
         allRecipes={allRecipes}
+        ingredient1={ingredient1}
+        ingredient2={ingredient2}
+        ingredient3={ingredient3}
+        recipesIngredient1={recipesIngredient1}
+        recipesIngredient2={recipesIngredient2}
+        recipesIngredient3={recipesIngredient3}
+        recipesIngredientResults={recipesIngredientResults}
+        rSelected={rSelected}
+        recipesResultsInput1andInput2={recipesResultsInput1andInput2}
       />
       <Footer />
     </div>
